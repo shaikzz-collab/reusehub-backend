8dca1e6dc680f2336d644ebcf17c0a8ba2db9558
Sameer <sameershaikz1114@gmail.com>
2025-08-08 12:18:21 +0530

    Integrated Cloudinary and removed local uploads folder

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -5,7 +5,7 @@ const { protect } = require('../middleware/authMiddleware');
 const upload = require('../cloudinary'); // Use new cloudinary upload
 
 // POST /api/items/upload → protected, with file upload
-router.post('/uploadS', protect, upload.single('image'), uploadItem);
+router.post('/uploads', protect, upload.single('image'), uploadItem);
 
 // GET /api/items → fetch all items
 router.get('/', getAllItems); // ✅ getAllItems must be a function
