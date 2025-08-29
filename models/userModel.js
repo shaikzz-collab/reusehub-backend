de1f2950c3f99f0d11db2a544c4cf0e8b585da83
Sameer <sameershaikz1114@gmail.com>
2025-08-29 22:58:00 +0530

    Fix: converted userModel to ESM with export default

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
